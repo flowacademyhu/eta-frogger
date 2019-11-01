7939c2cc141fd6befa8e1812de19e64120c81584
Tamas Vajgely <vajgi90@gmail.com>
2019-11-01 21:10:20 +0100

    Egy lehetséges megoldás a top3 pontszerző megjelenítésére

---
+++ topScorers.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+
+const leaderBoard = {
+  Klaudia: 39,
+  Isti: 17,
+  Peti: 15,
+  Máté: 19,
+  Smeagol: 30
+};
+
+const jsonBoard = JSON.stringify(leaderBoard);
+
+const save = () => {
+// Path néven majd változtatni kell!!!!!
+  fs.writeFile('/home/vajgi90/Flow/Frogger/eta-frogger/eta-frogger/result.json', jsonBoard, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Saving was succesful');
+  });
+};
+
+const fileLoader = () => {
+  //Path néven változtatni!!!!
+  const result = fs.readFileSync('/home/vajgi90/Flow/Frogger/eta-frogger/eta-frogger/result.json', 'utf8');
+};
+
+const topR = JSON.parse(result);
+
+const topRacers = (object) => {
+  let first = 0;
+  let second = 0;
+  let third = 0;
+  let firstRacer;
+  let secondRacer;
+  let thirdRacer;
+  for (const x in object) {
+    if (object[x] > first) {
+      third = second;
+      second = first;
+      first = object[x];
+      firstRacer = x;
+    } else if (object[x] > second) {
+      third = second;
+      second = object[x];
+      secondRacer = x;
+    } else if (object[x] > third) {
+      third = object[x];
+      thirdRacer = x;
+    }
+  }
+  console.log(`${firstRacer} : ${first} \n ${secondRacer} : ${second} \n ${thirdRacer} : ${third}`);
+};
+
+topRacers(topR);
+
+module.export = {
+  topRacers: topRacers,
+  save: save,
+  fileLoader: fileLoader
+};
